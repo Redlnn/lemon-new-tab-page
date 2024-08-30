90ea1f12c493c1160f9696ecac009de8de0985ac
Redlnn <w731347477@gmail.com>
2024-08-30 21:31:51 +0800

    update

--- entrypoints/newtab/changelog.ts
+++ entrypoints/newtab/changelog.ts
@@ -4,7 +4,8 @@ export default h('section', { class: 'update-content' }, [
   h('h3', null, '新特性'),
   h('ul', null, [
     h('li', null, '部分重构以支持 FireFox 浏览器'),
-    h('li', null, '支持缓存 Bing 每日一图')
+    h('li', null, '支持缓存 Bing 每日一图'),
+    h('li', null, '本地背景或已缓存的 Bing 壁纸新增进入动画'),
   ]),
   h('h3', null, '修复内容'),
   h('ul', null, [
