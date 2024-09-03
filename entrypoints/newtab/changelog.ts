a1a272e68c96b601e7a59d00b9708b56a6fc4e49
Redlnn <w731347477@gmail.com>
2024-09-03 22:29:13 +0800

    :bookmark: v1.5.1

--- entrypoints/newtab/changelog.ts
+++ entrypoints/newtab/changelog.ts
@@ -5,11 +5,12 @@ export default h('section', { class: 'update-content' }, [
   h('ul', null, [
     h('li', null, '部分重构以支持 FireFox 浏览器'),
     h('li', null, '支持缓存 Bing 每日一图'),
-    h('li', null, '本地背景或已缓存的 Bing 壁纸新增进入动画'),
+    h('li', null, '本地背景或已缓存的 Bing 壁纸新增进入动画')
   ]),
   h('h3', null, '修复内容'),
   h('ul', null, [
     h('li', null, '修复撤销取消置顶不生效的问题'),
-    h('li', null, '修复无法删除经常访问的问题 (#8)')
+    h('li', null, '修复无法删除经常访问的问题 (#8)'),
+    h('li', null, '修复【启用最常访问】开关不生效的问题')
   ])
 ])
