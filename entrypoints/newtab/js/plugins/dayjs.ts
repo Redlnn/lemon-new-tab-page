1ebc7a6eb35d1b960b9c44031d48170fbd7b022f
Redlnn <w731347477@gmail.com>
2024-09-06 22:12:23 +0800

    :globe_with_meridians: [i18n] dayjs

--- entrypoints/newtab/js/plugins/dayjs.ts
+++ entrypoints/newtab/js/plugins/dayjs.ts
@@ -3,6 +3,9 @@ import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 
 dayjs.extend(relativeTime)
-dayjs.locale('zh-cn')
+
+if (navigator.language.startsWith('zh')) {
+  dayjs.locale('zh-cn')
+}
 
 export default dayjs
