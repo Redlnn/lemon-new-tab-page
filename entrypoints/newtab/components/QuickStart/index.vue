4e6b4648e9b2b4cdc5a22c3cab456e713573d6cc
Redlnn <w731347477@gmail.com>
2025-03-12 22:39:00 +0800

    :recycle: 优化性能 & 增加注释

--- entrypoints/newtab/components/QuickStart/index.vue
+++ entrypoints/newtab/components/QuickStart/index.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ClearRound } from '@vicons/material'
 import type { TopSites } from 'wxt/browser'
-import _ from 'lodash'
 import { Pin16Regular, PinOff16Regular } from '@vicons/fluent'
 import { onMounted, ref, watch } from 'vue'
 import { useWindowSize } from '@vueuse/core'
@@ -30,45 +29,63 @@ const mounted = ref(false)
 const { width: windowWidth } = useWindowSize()
 
 function getContainerWidth(num: number) {
-  return (
+  const width =
     num * (15 + settingsStore.quickStart.itemSize + 15) +
-    (num - 1) * settingsStore.quickStart.itemMarginH +
-    40
-  )
+    (num - 1) * settingsStore.quickStart.itemMarginH
+  if (settingsStore.quickStart.showQuickStartContainerBg) {
+    return width + 40
+  }
+  return width
 }
 
 async function refresh() {
   await initBookmark()
-  const _bookmarks = _.cloneDeep(bookmarkStore.items)
+  // 先把书签单独存，避免计算过程直接对原 store 更改导致画面闪烁
+  const _bookmarks = bookmarkStore.items.slice()
+  let _topSites: TopSites.MostVisitedURL[] = []
 
-  let _topSites: TopSites.MostVisitedURL[] = await getTopSites()
-  if (_topSites === undefined || !settingsStore.quickStart.enableTopSites) {
-    _topSites = _bookmarks
+  if (settingsStore.quickStart.enableTopSites) {
+    //如果 getTopSites() 返回 undefined，默认空数组
+    _topSites = (await getTopSites()) ?? []
+
+    if (_topSites.length > 0) {
+      // 拿到书签里的url，比对最常访问里有没有重复的，有就去掉
+      const bookmarkUrlsSet = new Set(_bookmarks.map((item) => item.url))
+      _topSites = _topSites.filter((site) => !bookmarkUrlsSet.has(site.url))
+    }
   }
-  const bookmarkUrls = _bookmarks.map((item) => item.url)
-  _topSites = _topSites.filter((site) => !bookmarkUrls.includes(site.url))
 
+  // 计算快捷访问元素总数，为书签数+最常访问数+添加按钮
   const _itemCount = _bookmarks.length + _topSites.length + 1
 
+  // 当元素总数少于设置中的列数时，使实际列数=元素总数
   let _columnsCount = Math.min(settingsStore.quickStart.columns, _itemCount)
+  // 遍历列数，计算小于多少列时，容器大小不会超出页面
+  const containerWidth = windowWidth.value * 0.85
   for (let i = _columnsCount; i > 0; i--) {
-    if (getContainerWidth(i) < windowWidth.value * 0.85) {
+    if (getContainerWidth(i) < containerWidth) {
       _columnsCount = i
       break
     }
   }
 
   columnsNum.value = _columnsCount
-  if (_columnsCount <= settingsStore.quickStart.columns && _itemCount <= _columnsCount) {
+  // 当项目数量小于等于设置的列数时，行数为1
+  // 否则计算实际需要的行数
+  if (_itemCount <= _columnsCount) {
     rowsNum.value = 1
   } else {
+    // 讲元素总数÷实际列数（所需行数）与最大行数比较，取小的作为实际行数
     rowsNum.value = Math.min(settingsStore.quickStart.rows, Math.ceil(_itemCount / _columnsCount))
   }
 
+  // 定好行数和列数后才把书签上屏避免闪烁
   bookmarks.value = _bookmarks
-  const totalCellsNum = _columnsCount * rowsNum.value
+  // 计算实际可用的格子
+  const totalCellsNum = _columnsCount * rowsNum.value - 1
+  // 如果书签数量小于可用格子，就把最常访问填充剩余格子上屏
   if (_bookmarks.length < totalCellsNum) {
-    topSites.value = _topSites.slice(0, totalCellsNum - _bookmarks.length - 1)
+    topSites.value = _topSites.slice(0, totalCellsNum - _bookmarks.length)
   }
 }
 
