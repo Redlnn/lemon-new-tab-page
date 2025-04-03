f1585cfd037b98e7c51897b027147c85f5ad28c8
Redlnn <w731347477@gmail.com>
2025-04-03 23:42:28 +0800

    :hammer: 更新类型配置

--- types/wxt.d.ts
+++ types/wxt.d.ts
@@ -1,6 +1,3 @@
 /// <reference types="wxt/vite-builder-env" />
 /// <reference types="@wxt-dev/module-vue" />
 /// <reference types="@wxt-dev/i18n/module" />
-/// <reference types="./.wxt/types/paths.d.ts" />
-/// <reference types="./.wxt/types/i18n.d.ts" />
-/// <reference types="./.wxt/types/globals.d.ts" />
