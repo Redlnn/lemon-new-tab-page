1a2e2ed28ab248bdacfac70b648b0df6702a8404
Redlnn <w731347477@gmail.com>
2025-06-04 22:33:36 +0800

    :lipstick: 调整字体样式 (#26)

--- entrypoints/newtab/main.ts
+++ entrypoints/newtab/main.ts
@@ -1,26 +1,28 @@
 import { createPinia } from 'pinia'
 import { useDebounceFn } from '@vueuse/core'
 import { createApp, toRaw } from 'vue'
+import { browser } from 'wxt/browser'
 
 import './scripts/plugins/dayjs'
 import changeTheme from './scripts/use-element-plus-theme'
 import { i18n } from '@/.wxt/i18n'
-import { useFocusStore } from './scripts/store'
 import { initSettings, saveSettings, useSettingsStore } from './scripts/store/settingsStore'
 
 import './styles/index.scss'
 
 import App from './App.vue'
 
+const lang = browser.i18n.getUILanguage()
+
+document.documentElement.lang = lang
+document.title = i18n.t('newtab.title')
+
 const app = createApp(App)
 const pinia = createPinia()
-
-document.title = i18n.t('newtab.title')
 
 app.use(pinia)
 
 const settingsStore = useSettingsStore()
-const focusStore = useFocusStore()
 const saveSettingsDebounced = useDebounceFn(saveSettings, 100)
 
 initSettings().then(() => {
