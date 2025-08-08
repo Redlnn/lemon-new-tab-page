3528c12e6c788d4f8711faf721782fd4ed174f73
Redlnn <w731347477@gmail.com>
2025-08-09 00:11:56 +0800

    :sparkles: 在控制台输出banner

--- entrypoints/newtab/main.ts
+++ entrypoints/newtab/main.ts
@@ -3,6 +3,7 @@ import { useDebounceFn } from '@vueuse/core'
 import { createApp, toRaw } from 'vue'
 import { browser } from 'wxt/browser'
 
+import { version } from '@/package.json'
 import './scripts/plugins/dayjs'
 import changeTheme from './scripts/use-element-plus-theme'
 import { i18n } from '@/.wxt/i18n'
@@ -13,6 +14,44 @@ import 'element-plus/theme-chalk/el-message-box.css'
 import './styles/index.scss'
 
 import App from './App.vue'
+
+const banner = `
+
+%c Lemon New Tab %c ${version} %c
+
+                    ~~<~<~~<~<~~<~<~~<~~
+              <~~<~~<~{{{rhhhahhhhx}{{~<~~<~~<
+           <~<~<~|Uhhhahhhhao&W*ahhhhahhhU|~<~<~<
+        -~~<~?Chhhh     khkhh  hkhkk     hhhhC?~~<~-
+      +<~<~Jhhak   *hhhahhhhh  hhhahhhah*   kahhJ~~<~+
+     <~~<uhah   khhhahhhhahah  ahhhhahhhhhhk   hahu~<~<
+   <~<~thaa     hahhhahahhhha  hahahhhahahaa     ahht~<~<
+   <~~khh8 Bahhh  dahhhhhahha  hhhhhahhhad  hhhaB 8hhh~~~
+ ~~~_hah  hhhhahhhk  ahahhahh  ahahhhaa  khhhhhhhh  hhh_<~~
+~<~<Uhh  hhhahhhhhhh   ahhhha  hhhaha   hhhhahahahh  ahU<~<~
+~<~(ha  hhhahhahahahhhh  Maha  ahaM  hhhahhhhhhhhhhh  hh|~<~
+~~<hho  hhhhhhhhhhhahahhh          hhahhhhahahahahah  oak~~<
+<~~hh                                                  hh~<~
+~<~kao  hhhhhhhhhhhhhhhah          hhhhhhhhhhhhhhhhh  oah<~~
+~~<|hh  hhahahahahahahh  Mahh  hhaM  hahahahahahahah  hh|<~~
+<~~<Uhh  hhhhhhhhhhh   ahhhha  ahhhha   hhhhhhhhhhh  haU<~<~
+ ~<~_hah  hhahahahk  ahhhahha  hhahahha  khhahahah  hhh_~<~
+   ~<+khh8 Bahhh  dahhhahhahh  ahhhhhahhad  hhhaB 8hhh~~<
+   <~<~thha     ahhahhahhhhha  hhahahhhhahhh     ahht~<~<
+     <~~~uhah   khhhahhhahahh  ahhhhhahhhhak   hahv~~<~
+      +<~~~Jhhah   *hhahhhhha  hhahahhaa*   kahaJ<~<~+
+        -~<~~?Chhhh     hhhhh  hhhkk     hhhaJ?~~<~-
+           <~<~~~|Uhhhaakhkao&W*hhhhahhhhU|~<~~<<
+              <<~<~~~~{}{nhhhhhhahn}{{~~~~~<~<
+                    ~<~<~<~~~~~~<~<~<~<~
+`
+
+console.log(
+  banner,
+  'padding: 2px 6px; border-radius: 4px 0 0 4px; color: #fff; background: #ff9d00; font-weight: bold;',
+  'padding: 2px 6px; border-radius: 0 4px 4px 0; color: #fff; background: #ffbf00; font-weight: bold;',
+  'color: orange;line-height:0.8rem;'
+)
 
 let color = ''
 const lang = browser.i18n.getUILanguage()
