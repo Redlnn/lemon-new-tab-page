600bf755014cb92644d6b74b324847f7db13bc02
Redlnn <w731347477@gmail.com>
2025-09-18 21:31:51 +0800

    :wrench: 更新Typescript类型定义

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,11 +1,6 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-svg-loader" />
 /// <reference types="chrome" />
-
-declare module '*.svg?component' {
-  import type { FunctionalComponent, SVGAttributes } from 'vue'
-  const src: FunctionalComponent<SVGAttributes>
-  export default src
-}
 
 declare module 'jinrishici' {
   interface Origin {
@@ -41,11 +36,3 @@ declare module 'jinrishici' {
 
   export function load(result: (result: RootObject) => void, err: (error: Error) => void): void
 }
-
-type TupleIndices<T extends readonly unknown[]> = number extends T['length'] // 普通数组，长度不固定
-  ? number // 返回 number 类型
-  : Exclude<keyof T, keyof unknown[]> extends infer K
-    ? K extends `${infer N extends number}`
-      ? N
-      : never
-    : never
