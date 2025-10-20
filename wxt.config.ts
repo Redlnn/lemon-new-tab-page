38291d99b7dbfeec37ca3e2866fda2134693dbae
Redlnn <red_lnn@outlook.com>
2025-10-20 23:21:05 +0800

    :package: 调整打包策略缩小文件体积

--- wxt.config.ts
+++ wxt.config.ts
@@ -96,7 +96,32 @@ export default defineConfig({
       })
     ],
     build: {
-      sourcemap: false // for HMP (@wxt-dev/module-vue 会自动添加)
+      sourcemap: false, // for HMP (@wxt-dev/module-vue 会自动添加)
+      rollupOptions: {
+        output: {
+          // 将 SettingsDialog 相关组件及 Element Plus dialog 组件分离到独立 chunk
+          advancedChunks: {
+            groups: [
+              {
+                name: 'element-plus-components',
+                test: /node_modules\/element-plus\/es\/components/
+              },
+              {
+                name: 'settings-page',
+                test: /entrypoints\/newtab\/components\/SettingsPage/
+              },
+              {
+                name: 'search-box',
+                test: /entrypoints\/newtab\/components\/SearchBox/
+              },
+              {
+                name: 'shortcut',
+                test: /entrypoints\/newtab\/components\/Shortcut/
+              }
+            ]
+          }
+        }
+      }
     },
     resolve: {
       alias: {
