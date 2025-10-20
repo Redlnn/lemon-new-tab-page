428424dd9b0c38e1507eee7195c5edbfdb3d29f7
Redlnn <red_lnn@outlook.com>
2025-10-20 21:33:40 +0800

    :recycle: 删除unplugin-element-plus

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import Vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
-import ElementPlus from 'unplugin-element-plus/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 import Markdown from 'unplugin-vue-markdown/vite'
@@ -94,8 +93,7 @@ export default defineConfig({
       Components({
         resolvers: [elementPlusResolver],
         dts: 'types/components.d.ts'
-      }),
-      ElementPlus({})
+      })
     ],
     build: {
       sourcemap: false // for HMP (@wxt-dev/module-vue 会自动添加)
