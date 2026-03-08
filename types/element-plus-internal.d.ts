2c6ee36499fcfd7d95f4406b9147b405511f0146
Redlnn <red_lnn@outlook.com>
2026-03-08 23:22:07 +0800

    :arrow_up_small: 更新依赖，适配新版ElementPlus

---
+++ types/element-plus-internal.d.ts
@@ -0,0 +1,17 @@
+declare module 'element-plus/es/components/focus-trap/src/focus-trap.mjs' {
+  export { _default as default } from 'element-plus/es/components/focus-trap/src/focus-trap.vue'
+}
+
+declare module 'element-plus/es/components/teleport/src/teleport2.mjs' {
+  export { _default as default } from 'element-plus/es/components/teleport/src/teleport.vue'
+}
+
+declare module 'element-plus/es/components/dialog/src/dialog-content.mjs' {
+  import type { DefineComponent } from 'vue'
+  import type { DialogContentProps } from 'element-plus/es/components/dialog/src/dialog-content'
+
+  export const dialogContentEmits: { close: () => true }
+  export const dialogContentPropsDefaults: Omit<DialogContentProps, 'closeIcon'> & {
+    closeIcon?: string | DefineComponent
+  }
+}
